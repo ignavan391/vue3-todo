c0b31d09e0a777c2520094ccf433333e8037d15c
ignavan391 <ignatenkoivan61@gmail.com>
2020-09-30 12:06:03 +0600

    template ready

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import {store} from "./store";
 
+// import './assets/index.css'
+
 createApp(App)
   .use(store)
   .mount("#app");
+
+
+  
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,10 @@
 import {
   createStore,
-  createLogger,
   Store as VuexStore,
   CommitOptions,
-  DispatchOptions
-}
-  from "vuex";
+  DispatchOptions,
+  createLogger
+} from 'vuex'
 
 import { State, state } from './state'
 import { Mutations, mutations } from './mutations'
@@ -16,10 +15,18 @@ export const store = createStore<State>({
   plugins: process.env.NODE_ENV === 'development' ? [createLogger()] : [],
   state,
   mutations,
-  actions
-});
+  actions,
+  getters
+})
 
-export type Store = Omit<VuexStore<State>, 'getters' | 'commit' | 'dispatch'> & {
+export function useStore() {
+  return store as Store
+}
+
+export type Store = Omit<
+  VuexStore<State>,
+  'getters' | 'commit' | 'dispatch'
+> & {
   commit<K extends keyof Mutations, P extends Parameters<Mutations[K]>[1]>(
     key: K,
     payload: P,
@@ -28,11 +35,11 @@ export type Store = Omit<VuexStore<State>, 'getters' | 'commit' | 'dispatch'> & 
 } & {
   dispatch<K extends keyof Actions>(
     key: K,
-    payload : Parameters<Actions[K]>[1],
-    options?:DispatchOptions
+    payload?: Parameters<Actions[K]>[1],
+    options?: DispatchOptions
   ): ReturnType<Actions[K]>
 } & {
-  getters : {
+  getters: {
     [K in keyof Getters]: ReturnType<Getters[K]>
   }
 }
